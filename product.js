daf9876dbe1ce51d42251e2d9f2f3c0459a46ebc
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-15 23:56:41 +0100

    Add products

---
+++ product.js
@@ -0,0 +1,25 @@
+const products = [
+	{
+		id: 1,
+		name: "iPhone 12 pro",
+		desc: "6.1-inch display",
+		price: 999,
+		image: "https://res.cloudinary.com/keneogu/image/upload/v1680874266/onlineShop/iphone12_wdap1n.jpg"
+	},
+	{
+		id: 2,
+		name: "iPhone 13 pro",
+		desc: "5.4-inch display",
+		price: 699,
+		image: "https://res.cloudinary.com/keneogu/image/upload/v1680874313/onlineShop/iphone13_ripmmd.jpg"
+	},
+	{
+		id: 3,
+		name: "iPhone 14 pro",
+		desc: "6.9-inch display",
+		price: 799,
+		image: "https://res.cloudinary.com/keneogu/image/upload/v1680874315/onlineShop/iPhone14_le0urc.png"
+	},
+]
+
+module.exports = products
