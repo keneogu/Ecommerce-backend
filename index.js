10a65cfafc28befef2639dcb3ee09cbd87b038ba
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-20 21:12:54 +0100

    Add the cors middleware and access to products route

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const connectDb = require("./config/connectDb");
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+require('dotenv').config();
+
+connectDb()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(errorHandler)
+
+app.get("/", (req,res) => {
+	res.send("Welcome to our online shop API...")
+})
+
+app.use("/api/v1/products", require("./routes/productRoutes"))
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+	console.log(`Server running on app ${port}`)
+})
