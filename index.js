6634eb0d639d27a615f6264281129403d051eafc
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-21 22:05:41 +0100

    Add the user route middleware

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express")
+const express = require("express");
 const connectDb = require("./config/connectDb");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
-require('dotenv').config();
+require("dotenv").config();
 
-connectDb()
+connectDb();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(errorHandler)
+app.use(express.json());
+app.use(cors());
+app.use(errorHandler);
 
-app.get("/", (req,res) => {
-	res.send("Welcome to our online shop API...")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to our online shop API...");
+});
 
-app.use("/api/v1", require("./routes/productRoutes"))
+app.use("/api/v1", require("./routes/productRoutes"));
+app.use("/api/v1", require("./routes/userRoutes"));
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-	console.log(`Server running on app ${port}`)
-})
+  console.log(`Server running on app ${port}`);
+});
