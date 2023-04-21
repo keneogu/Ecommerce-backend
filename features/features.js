b9634a630c7b220dd0b76ca504b2c24fcdecb640
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-21 12:10:15 +0100

    Add filter function to filter categories

--- features/features.js
+++ features/features.js
@@ -16,6 +16,17 @@ class Features {
     this.query = this.query.find({ ...search });
     return this;
   };
+
+	filter = () => {
+		const queries = { ...this.queryStr };
+
+		const removeUndefinedQuery = ['search', 'limit', 'page']
+		removeUndefinedQuery.map(item => delete queries[item])
+
+
+		this.query = this.query.find(queries);
+		return this;
+	}
 }
 
 module.exports = Features;
