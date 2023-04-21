3852c408154f78c1982ec905e8fac135b6d495ea
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-21 11:35:58 +0100

    Update the getAllProducts to get the search product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,34 +1,57 @@
-const asyncHandler = require("express-async-handler")
-const Product = require("../models/productModel")
+const asyncHandler = require("express-async-handler");
+const Features = require("../features/features");
+const Product = require("../models/productModel");
 
-const getProducts = asyncHandler(async (req,res) => {
-	const products = await Product.find();
-	res.status(200).json(products)
+const getProducts = asyncHandler(async (req, res) => {
+  const features = new Features(Product.find(), req.query).search();
+  const products = await features.query;
+  res.status(200).json(products);
 });
 
-const createProduct = asyncHandler(async (req,res) => {
-	const product = await Product.create(req.body);
-	res.status(201).json(product);
-})
-
-const getProduct = asyncHandler(async (req,res) => {
-	const product = await Product.findById(req.params.id);
-	if(!product) {
-		res.status(404);
-		throw new Error("Product not found");
-	}
-	res.status(200).json(product);
+const createProduct = asyncHandler(async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
 });
 
-const updateProduct = asyncHandler(async (req,res) => {
-	const product = await Product.findById(req.params.id);
-	if(!product) {
-		res.status(404);
-		throw new Error("Product not found");
-	};
-
-	const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new:true});
-	res.status(200).json(updatedProduct);
+const getProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  res.status(200).json(product);
 });
 
-module.exports = {getProducts, createProduct, getProduct, updateProduct, deleteProduct}
+const updateProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedProduct);
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+
+  await Product.remove();
+  res.status(200).json(product);
+});
+
+module.exports = {
+  getProducts,
+  createProduct,
+  getProduct,
+  updateProduct,
+  deleteProduct,
+};
