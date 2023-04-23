3180b65fe87b239f8c3fe8f0a0f6a4c50aac41a4
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-23 16:39:27 +0100

    Authorize admin

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,11 +8,12 @@ const {
 } = require("../controllers/productController");
 
 const {authenticateUser} = require('../middleware/authenticate');
+const { authorize } = require('../middleware/authorize');
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-router.route("/admin/products/new").post(authenticateUser, createProduct);
+router.route("/admin/products/new").post(authenticateUser, authorize('admin'), createProduct);
 router.route("/product/:id").get(getProduct);
-router.route("/admin/product/:id").put(authenticateUser, updateProduct).delete(authenticateUser, deleteProduct);
+router.route("/admin/product/:id").put(authenticateUser, authorize('admin'), updateProduct).delete(authenticateUser, authorize('admin'), deleteProduct);
 
 module.exports = router;
