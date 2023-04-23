0670c6de846de4b41c0f576fea260fd0251fc046
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-23 15:04:48 +0100

    Add tokenHandler to users controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,63 +1,56 @@
-const User = require('../models/userModel');
-const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const tokenHandler = require("../middleware/tokenHandler");
 
-const registerUser = asyncHandler( async(req,res) => {
-	const {name,email,password} = req.body;
-	if(!name || !email || !password) {
-		res.status(400)
-		throw new Error("All fields are mandatory")
-	}
-	const userAvailable = await User.findOne({ email });
-	if(userAvailable) {
-		res.status(400)
-		throw new Error("User already exist")
-	}
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
 
-	const hashedPassword = await bcrypt.hash(password, 10);
-	console.log(hashedPassword);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
 
-	const user = await User.create({
-		name,
-		email,
-		password: hashedPassword,
-		avatar: {
-			public_id: 'Avatars/kene4_ryggsv',
-			url: 'https://res.cloudinary.com/keneogu/image/upload/v1682087314/Avatars/kene4_ryggsv.jpg'
-		}
-	});
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    avatar: {
+      public_id: "Avatars/kene4_ryggsv",
+      url: "https://res.cloudinary.com/keneogu/image/upload/v1682087314/Avatars/kene4_ryggsv.jpg",
+    },
+  });
 
-	const token = user.getJwtToken()
-
-	res.status(201).json({
-		success: true, 
-		token});
+  tokenHandler(user, 200, res);
 });
 
-const loginUser = asyncHandler(async(req,res) => {
-	const {email,password} = req.body;
-	
-	if(!email || !password) {
-		res.status(400)
-		throw new Error("All fields are mandatory")
-	}
-	const user = await User.findOne({email}).select('+password');
-	if(!user) {
-		res.status(401)
-		throw new Error("email or password not valid")
-	}
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-	const isPassword = await user.comparePassword(password)
-	if(!isPassword) {
-		res.status(401)
-		throw new Error("Incorrect Password")
-	}
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    res.status(401);
+    throw new Error("email or password not valid");
+  }
 
-	const token = user.getJwtToken()
+  const isPassword = await user.comparePassword(password);
+  if (!isPassword) {
+    res.status(401);
+    throw new Error("Incorrect Password");
+  }
 
-	res.status(200).json({token});
+	tokenHandler(user, 200, res);
+});
 
-})
-
-module.exports = {registerUser, loginUser}
+module.exports = { registerUser, loginUser };
