0f3956e0d9e4bcecf2b9315da33c79ec113ef816
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-23 22:58:25 +0100

    create user ref in the product model

--- controllers/productController.js
+++ controllers/productController.js
@@ -16,6 +16,7 @@ const getProducts = asyncHandler(async (req, res) => {
 });
 
 const createProduct = asyncHandler(async (req, res) => {
+  req.body.user = req.user.id
   const product = await Product.create(req.body);
   res.status(201).json(product);
 });
