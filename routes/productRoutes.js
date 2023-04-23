1d4017e083d8fe43970d0b4359f6af80c04d8797
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-23 15:20:25 +0100

    Authenicate routes for specific users

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,11 +6,13 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productController");
+
+const {authenticateUser} = require('../middleware/authenticate');
 const router = express.Router();
 
 router.route("/products").get(getProducts);
-router.route("/admin/products/new").post(createProduct);
+router.route("/admin/products/new").post(authenticateUser, createProduct);
 router.route("/product/:id").get(getProduct);
-router.route("/admin/product/:id").put(updateProduct).delete(deleteProduct);
+router.route("/admin/product/:id").put(authenticateUser, updateProduct).delete(authenticateUser, deleteProduct);
 
 module.exports = router;
