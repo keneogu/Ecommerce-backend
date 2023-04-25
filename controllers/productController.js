5705a47560b719cd10e6de77cac005068574902c
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-04-26 00:23:57 +0100

    Add create, update and delete review functionalities

--- controllers/productController.js
+++ controllers/productController.js
@@ -56,10 +56,84 @@ const deleteProduct = asyncHandler(async (req, res) => {
   res.status(200).json(product);
 });
 
+const createReview = asyncHandler(async(req,res) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    r => r.user.toString() === req.user._id.toString()
+  )
+
+  if(isReviewed) {
+    product.reviews.forEach(review => {
+      if(review.user.toString() === req.user.id.toString()) {
+        review.comment = comment;
+        review.rating = rating
+      }
+    })
+  }else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length
+  }
+
+  product.ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length
+
+  await product.save({ validateBeforeSvae: false });
+
+  res.status(200).json({
+    success: true
+  })
+});
+
+const getAllReviews = asyncHandler(async(req,res) => { 
+  const product = await Product.findById(req.query.id);
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews
+  })
+});
+
+const deleteReview = asyncHandler(async(req,res) => {
+  const product = await Product.findById(req.query.productId);
+
+  const reviews = product.reviews.filter(review => review._id.toString() !== req.query.id.toString());
+
+  const numOfReviews = reviews.length;
+
+  const ratings = product.reviews.reduce((acc, item) => item.rating + acc, 0) / reviews.length;
+
+  await Product.findByIdAndUpdate(req.query.productId, {
+    reviews,
+    ratings,
+    numOfReviews
+  }, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  })
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews
+  })
+});
+
 module.exports = {
   getProducts,
   createProduct,
   getProduct,
   updateProduct,
   deleteProduct,
+  createReview,
+  getAllReviews,
+  deleteReview
 };
