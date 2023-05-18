a823b25b5441fd6a3054fc742465b6069bc26901
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-18 11:20:04 +0100

    Add stripe precess Payment and send Payment actions

---
+++ controllers/paymentController.js
@@ -0,0 +1,22 @@
+const asyncHandler = require("express-async-handler");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.processPayment = asyncHandler(async (req, res, next) => {
+  const paymentIntent = await stripe.paymentIntent.create({
+    amount: req.body.amount,
+    currency: "usd",
+
+    metadata: { integration_check: "accept_a_payment" },
+  });
+
+  res.status(200).json({
+    success: true,
+    client_Secret: paymentIntent.client_Secret,
+  });
+});
+
+exports.sendPayment = asyncHandler(async (req, res, next) => {
+  res.status(200).json({
+    stripeApiKey: process.env.STRIPE_API_KEY,
+  });
+});
