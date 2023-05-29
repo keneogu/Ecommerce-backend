cf86b77e83ce1f45da286784391ac763fa469caa
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-05-29 14:25:39 +0100

    Add the admin routes to get the whole products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,6 +8,7 @@ const {
   createReview,
   getAllReviews,
   deleteReview,
+  getAdminProducts,
 } = require("../controllers/productController");
 
 const {authenticateUser} = require('../middleware/authenticate');
@@ -17,6 +18,7 @@ const router = express.Router();
 router.route("/products").get(getProducts);
 router.route("/admin/products/new").post(authenticateUser, authorize('admin'), createProduct);
 router.route("/product/:id").get(getProduct);
+router.route("/admin/products").get(getAdminProducts);
 router.route("/admin/product/:id").put(authenticateUser, authorize('admin'), updateProduct).delete(authenticateUser, authorize('admin'), deleteProduct);
 router.route('/review')
               .put(authenticateUser, createReview)
